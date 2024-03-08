03d48a8e705998641fee0fd3121f48337d9a5e41
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-03-08 00:11:08 +0000

    Update scripts_skater.js

--- assets/js/scripts_skater.js
+++ assets/js/scripts_skater.js
@@ -15,21 +15,10 @@ $('button.group').on('click', function() {
   // Hide/show relevant system versions
   $('.firmware .group').each(function (index, elem) {
     var no_kor = $(elem).hasClass('no_kor') && $('.kor').hasClass('selected');
-    if (no_kor) {
-      $(elem).removeClass('selected');
-      $(elem).hide();
-    } else {
-      $(elem).show();
-    }
-//    var eurjpn_only = $(elem).hasClass('eurjpn_only') && $('.eur').hasClass('selected') || $('.jpn').hasClass('selected');
-//    if (eurjpn_only) {
-//      $(elem).show();
-//    } else {
-//      $(elem).removeClass('selected');
-//      $(elem).hide();
-//    }
     var no_usa = $(elem).hasClass('no_usa') && $('.usa').hasClass('selected');
-    if (no_usa) {
+    var no_eur = $(elem).hasClass('no_eur') && $('.eur').hasClass('selected');
+    var no_jpn = $(elem).hasClass('no_jpn') && $('.jpn').hasClass('selected');
+    if (no_kor || no_usa || no_eur || no_jpn) {
       $(elem).removeClass('selected');
       $(elem).hide();
     } else {
