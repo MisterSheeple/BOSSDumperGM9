b34ca95399e38898bb4c197661f736aa62271853
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-03-08 00:15:29 +0000

    im a big idiot. i think this fixes everything

--- assets/js/scripts_skater.js
+++ assets/js/scripts_skater.js
@@ -14,15 +14,15 @@ $('button.group').on('click', function() {
 
   // Hide/show relevant system versions
   $('.firmware .group').each(function (index, elem) {
-    var no_kor = $(elem).hasClass('no_kor') && $('.kor').hasClass('selected');
-    var no_usa = $(elem).hasClass('no_usa') && $('.usa').hasClass('selected');
-    var no_eur = $(elem).hasClass('no_eur') && $('.eur').hasClass('selected');
-    var no_jpn = $(elem).hasClass('no_jpn') && $('.jpn').hasClass('selected');
-    if (no_kor || no_usa || no_eur || no_jpn) {
+    var kor_only = $(elem).hasClass('no_kor_only') && $('.kor').hasClass('selected');
+    var usa_only = $(elem).hasClass('usa_only') && $('.usa').hasClass('selected');
+    var eur_only = $(elem).hasClass('eur_only') && $('.eur').hasClass('selected');
+    var jpn_only = $(elem).hasClass('jpn_only') && $('.jpn').hasClass('selected');
+    if (kor_only || usa_only || eur_only || jpn_only) {
+      $(elem).show();
+    } else {
       $(elem).removeClass('selected');
       $(elem).hide();
-    } else {
-      $(elem).show();
     }
   })
 
