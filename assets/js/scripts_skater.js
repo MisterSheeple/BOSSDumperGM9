657840c2c8f31d4cbac794025999f4566a4f97f8
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-03-08 00:20:29 +0000

    Update scripts_skater.js

--- assets/js/scripts_skater.js
+++ assets/js/scripts_skater.js
@@ -13,12 +13,14 @@ $('button.group').on('click', function() {
   }
 
   // Hide/show relevant system versions
+ $('.btn-group .firmware').hide();
   $('.firmware .group').each(function (index, elem) {
     var kor_only = $(elem).hasClass('kor_only') && $('.kor').hasClass('selected');
     var usa_only = $(elem).hasClass('usa_only') && $('.usa').hasClass('selected');
     var eur_only = $(elem).hasClass('eur_only') && $('.eur').hasClass('selected');
     var jpn_only = $(elem).hasClass('jpn_only') && $('.jpn').hasClass('selected');
     if (kor_only || usa_only || eur_only || jpn_only) {
+      $('.btn-group .firmware').show();
       $(elem).show();
     } else {
       $(elem).removeClass('selected');
